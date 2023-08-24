1e83c2b3563b162b110330340ca60fe2bd18ce2f
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-24 17:24:18 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { of } from 'rxjs';
 import { takeLast } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/takelast
+// Example 1: take the last 3 emitted values before completion
+
 const srcList$ = of('Ignore', 'Ignore', 'Hello', 'World!');
 // take the last 2 emitted values
 const example = srcList$.pipe(takeLast(3));
